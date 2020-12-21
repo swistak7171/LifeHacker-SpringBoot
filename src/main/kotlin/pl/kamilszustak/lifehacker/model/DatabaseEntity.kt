bddfea24676ff5d948596f1663937ff3b4af58da
Kamil Szustak <kamil.szustak98@gmail.com>
2020-12-21 21:11:36 +0100

    Rename properties;

--- src/main/kotlin/pl/kamilszustak/lifehacker/model/DatabaseEntity.kt
+++ src/main/kotlin/pl/kamilszustak/lifehacker/model/DatabaseEntity.kt
@@ -1,5 +1,6 @@
 package pl.kamilszustak.lifehacker.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.util.*
@@ -9,13 +10,16 @@ import javax.persistence.*
 abstract class DatabaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     val id: Long = 0
 
     @CreatedDate
     @Column(name = "creation_date", nullable = false, updatable = false)
+    @JsonProperty("creation_date")
     val creationDate: Date = Date()
 
     @LastModifiedDate
     @Column(name = "modification_date", nullable = false)
+    @JsonProperty("modification_date")
     val modificationDate: Date = Date()
 }
--- src/main/kotlin/pl/kamilszustak/lifehacker/model/Lifehack.kt
+++ src/main/kotlin/pl/kamilszustak/lifehacker/model/Lifehack.kt
@@ -1,17 +1,25 @@
 package pl.kamilszustak.lifehacker.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
 
 @Entity
 @Table(name = "lifehacks")
 data class Lifehack(
+    @Column(name = "content")
+    @JsonProperty("content")
     val content: String = ""
 ) : DatabaseEntity() {
 
+    @Column(name = "rating")
+    @JsonProperty("rating")
     var rating: Double = 1.0
         private set
 
+    @Column(name = "rates_number")
+    @JsonProperty("rates_number")
     var ratesNumber: Int = 0
         private set
 
