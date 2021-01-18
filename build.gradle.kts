5fca55dafbf51e148c9d3afb664a1ea4ac9bd8ec
Kamil Szustak <kamil.szustak98@gmail.com>
2021-01-18 10:52:22 +0100

    Fix database configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.h2database:h2")
+    implementation("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
